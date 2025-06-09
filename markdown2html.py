8076c2542e97383d32836636154e07c4a497bf4f
Franklin11111 <abdulovnail6@gmail.com>
2025-06-09 18:32:12 +0400

    Added readme

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import os
+def markdown2html(markdown, output):
+    filename = 'README.md'
+    if not os.path.exists(filename):
+        print(f"Missing {filename}", file=sys.stderr)
+        exit(1)
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print(len(sys.argv))
+        exit(1)
+    else:
+        print()
+        exit(0)
+
+if __name__ == "__main__":
+    print(__import__("markdown2html").__doc__)
