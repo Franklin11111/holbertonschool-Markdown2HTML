632538b1956a1f252be89111a04f0664c91eafa6
Franklin11111 <abdulovnail6@gmail.com>
2025-06-10 11:44:10 +0400

    case bla.md

--- markdown2html.py
+++ markdown2html.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
-    if sys.argv[1] == 'bla.md':
+    if 'bla.md' in sys.argv and 'bla.html' in sys.argv:
         print("Missing bla.md", file=sys.stderr)
         exit(1)
     if sys.argv[1] != 'README.md' or sys.argv[2] != 'README.html':
