946b51656415c74a4b66d1455f310fb1174bae76
Franklin11111 <abdulovnail6@gmail.com>
2025-06-10 09:48:26 +0400

    Added readme

--- markdown2html.py
+++ markdown2html.py
@@ -7,9 +7,14 @@ import sys
 import os
 def markdown2html(markdown, output):
     # This function converts markdown to html
-
+    pass
     # if num_args < 2:
     #     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     filename = 'README.md'
     if not os.path.exists(filename):
         print(f"Missing {filename}", file=sys.stderr)
@@ -17,10 +22,6 @@ def markdown2html(markdown, output):
     else:
         print()
         exit(0)
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
 
     print(__import__("markdown2html").__doc__)
 
