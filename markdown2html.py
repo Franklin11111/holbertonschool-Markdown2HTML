e8a7f2bdca98f1377a7bbef2dea865ca74d4a61b
Franklin11111 <abdulovnail6@gmail.com>
2025-06-10 06:21:51 +0000

    add exit1

--- markdown2html.py
+++ markdown2html.py
@@ -11,11 +11,13 @@ def markdown2html(markdown, output):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
     filename = 'README.md'
     if not os.path.exists(filename):
         print(f"Missing {filename}", file=sys.stderr)
         exit(1)
     else:
+        print()
         exit(0)
 
 print(__import__("markdown2html").__doc__)
