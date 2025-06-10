77c2e1e19935df8bbaff2b92677f12926c53b2b6
Franklin11111 <abdulovnail6@gmail.com>
2025-06-10 10:32:29 +0400

    corrected filename check

--- markdown2html.py
+++ markdown2html.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
-    if sys.argv[1] == 'README.md' and sys.argv[2] == 'README.html':
+    if not sys.argv[1] == 'README.md' and sys.argv[2] == 'README.html':
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     filename = 'README.md'
