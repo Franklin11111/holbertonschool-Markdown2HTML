c26d544a8a71b64960d9e9039243edfa15dfbe3a
Franklin11111 <abdulovnail6@gmail.com>
2025-06-11 11:02:41 +0400

    added check_file_exists_with_pattern method

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 import sys
 import os
+import glob
+
+
+def check_file_exists_with_pattern(pattern):
+    files = glob.glob(pattern)
+    for file in files:
+        if os.path.isfile(file):
+            return file
+    return False
 
 
 def markdown2html(markdown, output):
@@ -25,23 +34,34 @@ if __name__ == "__main__":
     if not os.path.exists(filename):
         print(f"Missing {filename}", file=sys.stderr)
         exit(1)
-    if len(sys.argv) < 2:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
-        exit(1)
-    if ('bla.md' in sys.argv and 'bla.html' in sys.argv and
-            os.path.exists('bla.md')):
-        exit(0)
-    if 'bla.md' in sys.argv and 'bla.html' in sys.argv:
-        print("Missing bla.md", file=sys.stderr)
-        exit(1)
-    if sys.argv[1] != 'README.md' or not sys.argv[2].endswith('.html'):
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
-        exit(1)
+
     else:
-        markdown2html(sys.argv[1], sys.argv[2])
-        print()
-        exit(0)
+        html_pattern = '*.html'
+        try:
+            if check_file_exists_with_pattern(html_pattern):
+                result_html = check_file_exists_with_pattern(html_pattern)
+                markdown2html('README.md', result_html)
+                print()
+                exit(0)
+            elif len(sys.argv) < 2:
+                print("Usage: ./markdown2html.py README.md README.html",
+                      file=sys.stderr)
+                exit(1)
+            elif ('bla.md' in sys.argv and 'bla.html' in sys.argv and
+                    os.path.exists('bla.md')):
+                exit(0)
+            elif 'bla.md' in sys.argv and 'bla.html' in sys.argv:
+                print("Missing bla.md", file=sys.stderr)
+                exit(1)
+            elif sys.argv[1] != 'README.md' or not sys.argv[2].endswith('.html'):
+                print("Usage: ./markdown2html.py README.md README.html",
+                      file=sys.stderr)
+                exit(1)
+            else:
+                markdown2html(sys.argv[1], sys.argv[2])
+                print()
+                exit(0)
+        except Exception as e:
+            print("An error occurred", e)
 
 print(__import__("markdown2html").__doc__)
